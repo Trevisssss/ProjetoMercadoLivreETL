4dd3a6d08e86b8345d52ae580c66fd8f3a4239fe
Trevisssss <trevisan_bruno@hotmail.com>
2025-04-14 15:56:41 -0300

    updates

--- coleta/coleta/spiders/notebook.py
+++ coleta/coleta/spiders/notebook.py
@@ -5,13 +5,31 @@ class NotebookSpider(scrapy.Spider):
     name = "notebook"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/notebooks#D[A:notebook]"]
+    
+    page_count = 1 # Initialize page count to 1
+    max_page = 10 # Set the maximum number of pages to scrape
 
     def parse(self, response):
+        
         products = response.css('div.ui-search-result__wrapper')
 
         for product in products:
+
+            prices = product.css('span.andes-money-amount__fraction::text').getall()
+
             yield {
                 "brand": product.css('span.poly-component__brand::text').get(),
                 "name" : product.css('a.poly-component__title::text').get(),
-                "rating": product.css('span.poly-reviews__rating::text').get()
+                "reviews_rating": product.css('span.poly-reviews__rating::text').get(),
+                "reviews_count": product.css('span.poly-reviews__total::text').get(),
+                "price": prices[0] if len(prices) > 0 else None,
+                "price_discounted": prices[1] if len(prices) > 1 else None,
             }
+
+        if self.page_count < self.max_page:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                self.page_count += 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
+
+        
